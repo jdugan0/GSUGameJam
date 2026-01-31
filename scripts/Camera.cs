47a7ad8c764d51bd2dc57c36b2d6c160933ff920
jdugan35 <jdugan35@gatech.edu>
2026-01-30 19:49:54 -0500

    meow

--- scripts/Camera.cs
+++ scripts/Camera.cs
@@ -3,11 +3,15 @@ using Godot;
 
 public partial class Camera : Camera2D
 {
-    [Export]
     Movement player;
 
     [Export]
     int playerWeight;
+
+    public override void _Ready()
+    {
+        player = GetTree().GetFirstNodeInGroup("Player") as Movement;
+    }
 
     public override void _Process(double delta)
     {
