d8816a8854404ccca45863421e2600a3646e1d38
DiscountGrocery <austinmnewby@gmail.com>
2026-01-31 21:18:56 -0500

    level test and enemy dir fix

--- scripts/Enemy.cs
+++ scripts/Enemy.cs
@@ -147,20 +147,11 @@ public partial class Enemy : CharacterBody2D
             Vector2 dir = (next - GlobalPosition).Normalized();
             navigationAgent2D.Velocity = dir * maxSpeed;
             Velocity = computedV;
-            if (Velocity.Length() <= 10f)
-            {
-                globalAngle = computedV.Angle();
-            }
         }
         else if (!stunned)
         {
             navigationAgent2D.Velocity = Vector2.Zero;
             Velocity = computedV;
-            
-            if (Velocity.Length() <= 10f)
-            {
-                globalAngle = computedV.Angle();
-            }
         }
         else if (stunned)
         {
@@ -174,7 +165,19 @@ public partial class Enemy : CharacterBody2D
                 }
             }
         }
-        //GD.Print(computedV.Angle());
+        if (Velocity.Length() >= 10f)
+        {
+            globalAngle = computedV.Angle();
+        }
+        if (Velocity.X < 0)
+        {
+            enemySprite.FlipH = true;
+        }
+        else
+        {
+            enemySprite.FlipH = false;
+        }
+        GD.Print(globalAngle);
         UpdateRotation();
         MoveAndSlide();
     }
