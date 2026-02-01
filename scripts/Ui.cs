3022cc0a41893eaaed1aca533080425384f765fa
jdugan35 <jdugan35@gatech.edu>
2026-01-31 19:40:46 -0500

    pause basics

--- scripts/Ui.cs
+++ scripts/Ui.cs
@@ -15,6 +15,9 @@ public partial class Ui : CanvasLayer
     [Export]
     public Label timer;
 
+    [Export]
+    CanvasLayer pauseMenu;
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
